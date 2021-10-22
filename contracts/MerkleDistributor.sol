26acff3f5a6b41ff767ad315cce009ebaea79fa3
Lei Ding <dinn@duedex.com>
2021-10-22 11:29:41 +0800

    refactor

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -7,7 +7,6 @@ import '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
 import '@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol';
 import '@openzeppelin/contracts/utils/introspection/IERC165.sol';
 import '@openzeppelin/contracts/utils/cryptography/MerkleProof.sol';
-import '@openzeppelin/contracts/access/Ownable.sol';
 
 contract MerkleDistributor is IERC1155Receiver {
 	IERC20 public immutable token;
